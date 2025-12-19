2fef382a6ccb791fb8f34032ad365c0a3b41d6d9
RedouaneEn-nminej <redouneennminej2005@gmail.com>
2025-12-19 19:48:27 +0100

    modif sapabase file

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,14 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  console.error('❌ Supabase env vars missing');
-}
-
-export const supabase = createClient(
-  supabaseUrl ?? '',
-  supabaseAnonKey ?? ''
-);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export interface Comment {
   id: string;
